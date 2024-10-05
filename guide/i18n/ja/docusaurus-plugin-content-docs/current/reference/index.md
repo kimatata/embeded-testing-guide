9e26a6bf2cdfa5a8c8d0e8cb6032c16e5353b64f
Takeshi Kimata <117462761+kimatata@users.noreply.github.com>
2024-10-05 21:33:52 +0900

    change guide structure

--- guide/i18n/ja/docusaurus-plugin-content-docs/current/reference/index.md
+++ guide/i18n/ja/docusaurus-plugin-content-docs/current/reference/index.md
@@ -5,30 +5,30 @@ import atGoogle from "./img/atGoogle.png"
 
 # 参考文献
 
-### Test Driven Development for Embedded C
+## Test Driven Development for Embedded C
 
 <img src={bookEmbeded} alt="" width="150" />
 
 [https://www.oreilly.com/library/view/test-driven-development/9781941222997/](https://www.oreilly.com/library/view/test-driven-development/9781941222997/)
 
-### 単体テストの考え方/使い方
+## 単体テストの考え方/使い方
 
 <img src={unitTesting} alt="単体テストの考え方/使い方" width="150" />
 
 [https://book.mynavi.jp/ec/products/detail/id=134252](https://book.mynavi.jp/ec/products/detail/id=134252)
 
-### ソフトウェア品質を高める開発者テスト 改訂版 アジャイル時代の実践的・効率的でスムーズなテストのやり方
+## ソフトウェア品質を高める開発者テスト 改訂版 アジャイル時代の実践的・効率的でスムーズなテストのやり方
 
 <img src={bookDevelopment} alt="ソフトウェア品質を高める開発者テスト 改訂版 アジャイル時代の実践的・効率的でスムーズなテストのやり方" width="150" />
 
 [https://www.shoeisha.co.jp/book/detail/9784798176390](https://www.shoeisha.co.jp/book/detail/9784798176390)
 
-### Software Engineering at Google
+## Software Engineering at Google
 
 <img src={atGoogle} alt="Software Engineering at Google" width="150" />
 
 [https://abseil.io/resources/swe-book](https://abseil.io/resources/swe-book)
 
-### Google Test
+## Google Test
 
 [https://google.github.io/googletest/](https://google.github.io/googletest/)
--- guide/src/pages/index.tsx
+++ guide/src/pages/index.tsx
@@ -1,16 +1,16 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Heading from "@theme/Heading";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero', styles.heroBanner)}>
+    <header className={clsx("hero", styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
@@ -18,9 +18,16 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
+            className="button button--primary button--lg"
+            to="/docs/intro/introduction"
+          >
+            📖Guide
+          </Link>
+          <Link
             className="button button--secondary button--lg"
-            to="/docs/intro/introduction">
-            Move to guide
+            to="https://github.com/kimatata/embeded-testing-guide"
+          >
+            GitHub
           </Link>
         </div>
       </div>
@@ -29,11 +36,12 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
